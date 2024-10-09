c9fe1bae70d169e5485d94329bb46e80468073e1
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-10-08 20:09:25 -0400

    Add code to test set/get functions

--- hash-map-practice.js
+++ hash-map-practice.js
@@ -1,16 +1,16 @@
 class HashMap {
     constructor(size = 16) {
-        this.buckets = new Array(size);
-        this.size = size;
+        this.buckets = new Array(size)
+        this.size = size
     }
 
     // takes a key and produces a hash code
     hash(key) {
-        let hashCode = 0;
+        let hashCode = 0
 
-        const primeNumber = 31;
+        const primeNumber = 31
         for (let i = 0; i < key.length; i++) {
-            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode = primeNumber * hashCode + key.charCodeAt(i)
         }
 
         return hashCode % this.size
@@ -18,25 +18,25 @@ class HashMap {
 
     // takes 2 arguments, first is the key, second is the value assigned to that key. If the key already exist, it shouuld update the key's value with the new value.
     set(key, value) {
-        let index = this.hash(key);
+        let index = this.hash(key)
 
-        if(!this.buckets[index]) {
-            this.buckets[index] = [];
+        if (!this.buckets[index]) {
+            this.buckets[index] = []
         }
 
-        this.buckets[index].push([key, value]);
+        this.buckets[index].push([key, value])
     }
 
     // takes one argument as a kay and returns the value that is assigned to this key, if key is not found, return null
     get(key) {
-        let index = this.hash(key);
+        let index = this.hash(key)
 
         if (!this.buckets[index]) {
-            return null;
+            return null
         }
-
+        
         for (let bucket of this.buckets[index]) {
-            if(bucket[0] === key) {
+            if (bucket[0] === key) {
                 return bucket[1]
             }
         }
@@ -77,3 +77,13 @@ class HashMap {
 
     }
 }
+
+const test = new HashMap()
+
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+
+
+console.log(test.get('dog'))
